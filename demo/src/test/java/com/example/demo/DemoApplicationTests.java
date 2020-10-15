4f5bf40e8e0348e53ced6488ea588ca70b3e6c20
sho-dang <70437690+sho-dang@users.noreply.github.com>
2020-10-15 21:48:45 +0900

    test実装、混乱中

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class DemoApplicationTests {
-
-
+    public static void main(String[] args) {
+    SpringApplication.run(DemoApplicationTests.class, args);
+    }
 }
--- demo/src/test/java/com/example/demo/MapperTest/AppMapperTest.java
+++ demo/src/test/java/com/example/demo/MapperTest/AppMapperTest.java
@@ -1,24 +1,40 @@
 package com.example.demo.MapperTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+
 
 import com.example.demo.Mapper.AppMapper;
 import com.example.demo.domain.DateList;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = AppMapper.class)
+//@TestPropertySource(locations = "test.properties")
+@SpringBootTest(properties = "spring.main.web-application-type=reactive")
 public class AppMapperTest {
 
     @Autowired
-      AppMapper appM;
+     private AppMapper appM;
+     
+    
+
+
 
      @Test
      public void DBのデータを全件取得する() throws Exception{
          List<DateList> allData = appM.datelist();
-         
+         assertEquals(allData.size(), 4 );
      }
+
 
 
 }
