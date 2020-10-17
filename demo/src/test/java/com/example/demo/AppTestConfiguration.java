c22f7d24a8cd88931002a0e82e0715a9063c5bb7
sho-dang <70437690+sho-dang@users.noreply.github.com>
2020-10-17 22:39:37 +0900

    test実装学習中

--- demo/src/test/java/com/example/demo/AppTestConfiguration.java
+++ demo/src/test/java/com/example/demo/AppTestConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
-
+import java.sql.*;
 //@Configuration
 public class AppTestConfiguration {
+    public static void main(String[] args)throws Exception {
+Connection conn = DriverManager.
+    getConnection("jdbc:h2:~/test", "sa", "");
+// add application code here
+conn.close();
 
-
-    
+}
 }
