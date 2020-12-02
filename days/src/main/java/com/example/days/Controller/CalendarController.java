c1bc8749afebc472a5452280ec71ee2d291f7b7a
sho <s-_-s-_-s.0e1b2i5.python@ezweb.ne.jp>
2020-12-02 22:05:40 +0900

    一斉休暇表示デザイン変更/休暇者登録実装中

--- days/src/main/java/com/example/days/Controller/CalendarController.java
+++ days/src/main/java/com/example/days/Controller/CalendarController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/list")
@@ -22,28 +24,39 @@ public class CalendarController {
 
     @GetMapping
     public String createTable(Model model){
+
+
         List<DayList> days = service.days();
         DayMarge init = new DayMarge(days);
+        model.addAttribute("days", init.getDayList());
+
+
         List<DayList>  li = new ArrayList<>();
+        init.getDayList().stream()
+                .filter(d -> d.getWorkId().equals("9999"))
+                .forEach(d -> li.add(d));
+        model.addAttribute("holiday",li);
 
-        init.getDayList().stream().filter(d -> d.getWorkId().equals("9999"))
-                    .forEach(d -> li.add(d));
-
+/*
+        //
         String[] shiftNameList = {"シフト1","シフト2","シフト3","シフト4"};
         List<NameList> shiftListOne = service.listOne(shiftNameList[0]);
         List<NameList> shiftListTwo = service.listOne(shiftNameList[1]);
         List<NameList> shiftListThree = service.listOne(shiftNameList[2]);
         List<NameList> shiftListFour = service.listOne(shiftNameList[3]);
-
-        model.addAttribute("days", init.getDayList());
-        model.addAttribute("holiday",li);
+        
         model.addAttribute("one", shiftListOne);
         model.addAttribute("two", shiftListTwo);
         model.addAttribute("three", shiftListThree);
         model.addAttribute("four", shiftListFour);
+*/
 
         return "calendar";
     }
-    
+    @PostMapping("/list/{vacation}")
+    public String nameSet(@PathVariable("vacation") String vacation){
+        
+        return "redirect:/calendar";
+    }
 
 }
