e2f8675d24712ad6f6cd7f7030edcd4904de40ed
sho <s-_-s-_-s.0e1b2i5.python@ezweb.ne.jp>
2020-12-04 17:18:23 +0900

    休暇者名が取得できる/DB日付テーブルデータ変更

--- days/src/main/java/com/example/days/domain/Day/DayList.java
+++ days/src/main/java/com/example/days/domain/Day/DayList.java
@@ -4,11 +4,25 @@ public class DayList {
     
     private int date;
     private String workId;
+    private String vacationCode;
+
     private String shiftOne;
     private String shiftTwo;
     private String shiftThree;
     private String shiftFour;
     
+    private String vacationNameOne;
+    private String overNameOne;
+    private String earlyNameOne;
+
+    private String vacationNameTwo;
+    private String overNameTwo;
+    private String earlyNameTwo;
+
+    private String vacationNameThree;
+    private String overNameThree;
+    private String earlyNameThree;
+
     
 
     public int getDate(){
@@ -17,6 +31,10 @@ public class DayList {
     public String getWorkId(){
         return this.workId;
     }
+    public String getVacationCode(){
+        return this.vacationCode;
+    }
+    
     public String getShiftOne(){
         return this.shiftOne;
     }
@@ -29,6 +47,37 @@ public class DayList {
     public String getShiftFour(){
         return this.shiftFour;
     }
+    
+    public String getVacationNameOne() {
+        return vacationNameOne;
+    }
+    public String getVacationNameTwo() {
+        return vacationNameTwo;
+    }
+    public String getVacationNameThree() {
+        return vacationNameThree;
+    }
+    public String getOverNameOne() {
+        return overNameOne;
+    }
+    public String getOverNameTwo() {
+        return overNameTwo;
+    }
+    public String getOverNameThree() {
+        return overNameThree;
+    }
+    public String getEarlyNameOne() {
+        return earlyNameOne;
+    }
+    public String getEarlyNameTwo() {
+        return earlyNameTwo;
+    }
+    public String getEarlyNameThree() {
+        return earlyNameThree;
+    }
+
+
+
 
     public void setDate(int date){
         this.date = date;
@@ -36,6 +85,10 @@ public class DayList {
     public void setWorkId(String workId){
         this.workId = workId;
     }
+    public void setVacationCode(String vacationCode){
+        this.vacationCode = vacationCode;
+    }
+
     public void setShiftOne(String shiftOne){
         this.shiftOne = shiftOne;
     }
@@ -49,4 +102,31 @@ public class DayList {
         this.shiftFour = shiftFour;
     }
 
+    public void setVacationNameOne(String vacationNameOne) {
+        this.vacationNameOne = vacationNameOne;
+    }
+    public void setVacationNameTwo(String vacationNameTwo) {
+        this.vacationNameTwo = vacationNameTwo;
+    }
+    public void setVacationNameThree(String vacationNameThree) {
+        this.vacationNameThree = vacationNameThree;
+    }
+    public void setOverNameOne(String overNameOne) {
+        this.overNameOne = overNameOne;
+    }
+    public void setOverNameTwo(String overNameTwo) {
+        this.overNameTwo = overNameTwo;
+    }
+    public void setOverNameThree(String overNameThree) {
+        this.overNameThree = overNameThree;
+    }
+    public void setEarlyNameOne(String earlyNameOne) {
+        this.earlyNameOne = earlyNameOne;
+    }
+    public void setEarlyNameTwo(String earlyNameTwo) {
+        this.earlyNameTwo = earlyNameTwo;
+    }
+    public void setEarlyNameThree(String earlyNameThree) {
+        this.earlyNameThree = earlyNameThree;
+    }
 }
