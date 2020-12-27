bdcd8dad91ca72f42a76ed1c329f39a5afbd26f5
sho <s-_-s-_-s.0e1b2i5.python@ezweb.ne.jp>
2020-12-27 19:52:48 +0900

    Theories/Theoryテスト実装

--- days/src/test/java/com/example/days/Tests/ServiceTest/AppServiceTest.java
+++ days/src/test/java/com/example/days/Tests/ServiceTest/AppServiceTest.java
@@ -2,6 +2,8 @@ package com.example.days.Tests.ServiceTest;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assume.assumeThat;
+import static org.mockito.Mockito.calls;
 
 import java.util.List;
 
@@ -9,18 +11,32 @@ import com.example.days.Service.AppService;
 import com.example.days.domain.Day.DayList;
 import com.example.days.domain.NameList.NameList;
 
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
-
+/*
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppServiceTest {
-    @Autowired
     public AppService service;
+
+        @Before
+        public void initialize(){
+        service = new AppService();
+        }
 
     @Test
     public void ーnameListAllメソッドーnameListを全件取得できていること()throws Exception{
@@ -122,10 +138,81 @@ public class AppServiceTest {
                         dayFour.substring(0,1) +
                         dayFive.substring(0,1);
         assertThat(code, is(changedCode));
-    }
+    } 
     
-    @Test //未実装
-    public void ーupdateMethodメソッドー()throws Exception{
-
-    }
 }
+*/
+
+@RunWith(Theories.class)
+@SpringBootTest
+    public class AppServiceTest{
+        @Autowired
+        AppService service;
+
+        @ClassRule
+        public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+        @Rule
+        public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+        /**
+         * テストデータ
+         * param 日付
+         */
+        @DataPoints
+        public static DateFixture[] Param = {
+            new DateFixture(1,"11433",new int[]{25,26},"1234"),//4月
+            new DateFixture(1,"44222",new int[]{2,3,4,5,6},"4132"),//5月
+            new DateFixture(1,"42222",new int[]{7,8,9,10,11,12,13,14,15,16},"1234"),//8月
+            new DateFixture(4,"11143",new int[]{1,2,3},"4213"),//1月
+        };
+        
+        @Theory
+        public void 一斉休暇日を変更した後出勤日の自動計算ができていること(DateFixture p)throws Exception{
+            
+            for(int i = p.休暇日[0] ; i < p.休暇日[0]+p.休暇日.length ; i++){
+                service.updateAllVacation(i);
+            };
+            service.updateWorkId(p.日付,p.出勤形態);
+
+            //休暇日が無い20日を検証
+            DayList workCode = service.selectDayList(20);
+            List<DayList> uo = service.days();
+
+            //休暇日初期化
+            for(int i = p.休暇日[0] ; i < p.休暇日[0]+p.休暇日.length ; i++){
+                service.deleteAllVacation(i);
+            };
+            //assumeThat(, actual, matcher);
+            //assertThat(service.selectDayList(p.日付).getDate(), is(p.日付));
+            assertThat(workCode.getWorkId(),is(p.期待値));
+            
+        }
+
+        //@Ignore("its Fixture class")
+        static class DateFixture{
+            /** 日付
+             *  1日〜30日(11月)
+             */
+            int 日付;
+            /** 出勤形態
+             *     1 = 1直
+             *     2 = 2直
+             *     3 = 3直
+             *     4 = 休み
+             */
+            String 出勤形態; 
+
+            int[] 休暇日; 
+
+            String 期待値;
+
+            DateFixture(int 日付,String 出勤形態,int[] 休暇日,String 期待値){
+                this.日付 = 日付;
+                this.出勤形態 = 出勤形態;
+                this.休暇日 = 休暇日;
+                this.期待値 = 期待値;
+            }
+        }
+    }
+
